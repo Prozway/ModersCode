c89b114cc765e428268ddab316d8e8e5051e999f
Prozway <111115961+Prozway@users.noreply.github.com>
2022-11-14 20:24:34 +0100

    Create QuickCupScript.py

---
+++ QuickCupScript.py
@@ -0,0 +1,111 @@
+#Inspired by C and CupScript (Veiw CupScript here! https://github.com/Fus3n/cupscript?adlt=strict&toWww=1&redig=4B4AF9F6DBC247098C7C8BEC99A061A5)
+
+
+#Put imports here
+import time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int_printf = print
+
+
+ask = input
+ask_main = ask
+
+#x = ask_main_user("What would you like to do? ")
+#x = amu("Asking with shortcut >> ")
+
+get_main = input
+
+frommain = 'frommainna'
+
+KeyErrorMain = KeyError
+
+main_filter = filter
+
+main_any_ = any
+
+main_program_wait = time.sleep
+
+main_program_wait(10)
+
+loop_i = 0 #not nil because nil has not been defined yet!
+
+def ErrorQCS():
+    for i in range(1,99999999999999999999,1):
+        int_printf("QuickCupScript")
+        main_program_wait(0.1)
+        int_printf("             QuickCupScript")
+        main_program_wait(0.1)
+        int_printf("                                QuickCupScript")
+        main_program_wait(0.1)
+
+nil = 0
+main_nil = 0.00000000001 #Ten zeros!!!
+inf = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999 #if in secconds this is more then 100 years. 
+
+def studio_include():
+    int_printf("<Studio_Iclude>")
+    main_program_wait(0.5)
+    int_printf("Waiting...")
+    main_program_wait(10)
+    int_printf("<Studio_Include> {<S_I Ready for use>} ")
+    def Err_Solve():
+        int_printf("[Err_Solve] >> Program ended due to Err_Solve Command")
+        int_printf("'[Err_Solve]' >> Programa finalizado debido a Err_Solve comando")
+        main_program_wait(0.5)
+        int_printf("'[Err_Solve]' Is running please restart the program. ")
+        for loop_i in range():
+            int_printf("Running Stop Process...")
+            break
+        main_program_wait(999999999999999999999999999999999999999999999999)
+    def time_inf_wait_main():
+        main_program_wait(99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999*999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999) #This is very big its about more then a year
